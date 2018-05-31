e9956564b3b76f3f0cd7108f4f1fbd61fddd5ec5
kiruba <kiruba@adapt.io>
2018-05-31 22:29:06 +0530

    UI fixes

--- client/components/Home/innerSections/clientsCorner.jsx
+++ client/components/Home/innerSections/clientsCorner.jsx
@@ -20,25 +20,25 @@ class ClientsCorner extends React.Component {
                 </h3>
               </div>
               <div className="row client-logos-wrapper">
-                <div className="col-sm-12 col-md-6 col-lg-3 disp-vertical-center">
+                <div className="col-sm-12 col-md-6 col-lg-3 disp-vertical-center d-sm-ad">
                   <img
                     src="http://domains.whoswho/sites/default/files/images/ww-logo-excellence-v2-OG.png"
                     alt=""
                   />
                 </div>
-                <div className="col-sm-12 col-md-6 col-lg-3 disp-vertical-center">
+                <div className="col-sm-12 col-md-6 col-lg-3 disp-vertical-center d-sm-ad">
                   <img
                     src="https://greensock.com/wp-content/uploads/2014/02/logo-youtube.png"
                     alt=""
                   />
                 </div>
-                <div className="col-sm-12 col-md-6 col-lg-3 disp-vertical-center">
+                <div className="col-sm-12 col-md-6 col-lg-3 disp-vertical-center d-sm-ad">
                   <img
                     src="https://marketing.dcassetcdn.com/blog/2016/January/top-company-logos-one-color/17_Nestle_400.png"
                     alt=""
                   />
                 </div>
-                <div className="col-sm-12 col-md-6 col-lg-3 disp-vertical-center">
+                <div className="col-sm-12 col-md-6 col-lg-3 disp-vertical-center d-sm-ad">
                   <img
                     src="https://d2slcw3kip6qmk.cloudfront.net/marketing/blogs/press/brand-identity-design/apple-logo.png"
                     alt=""
