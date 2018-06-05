2183c01c736230073b5082d4165f669403a6a8e9
loksbalaji <loksbalaji@gmail.com>
2018-06-05 08:05:21 +0530

    home page ui initial

--- client/components/Header/menu.jsx
+++ client/components/Header/menu.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import classnames from "classnames";
 
 import style from "./style.css";
+import { CSSTransitionGroup } from "react-transition-group";
 
 const _img_closebut = require("../../images/menu-close.png");
 const _img_menu_watermark = require("../../images/menu-bg.jpg");
@@ -29,44 +30,53 @@ class Menu extends React.Component {
             (this.props.dispProp ? "display-block" : "display-none")
           }
         >
-          <div className="menu-wrapper violet-gradientbg-light">
-            <div className="menu-wrapper-bg-img" style={_style_menu_wrapper} />
-            <div className="container">
-              <div className="row justify-content-center menu-content-box">
-                <div className="pos-absolute close-button-holder">
-                  <a
-                    href="javascript:"
-                    className="close-img"
-                    onClick={() => this.closeMenu()}
-                  >
-                    <img src={_img_closebut} alt="" />
-                  </a>
-                </div>
-                <div className="col-12">
-                  <ul>
-                    <li>
-                      <Link to="/team">About Us</Link>
-                    </li>
-                    <li>
-                      <Link to="/careers">Careers</Link>
-                    </li>
-                    <li>
-                      <Link to="/faq">FAQ</Link>
-                    </li>
-                    <li>
-                      <Link to="javascript:">Blog</Link>
-                    </li>
-                    <li>
-                      <Link to="/contractor-signup">Become a contractor</Link>
-                    </li>
-                    <li>
-                      <Link to="/contact">Contact Us</Link>
-                    </li>
-                  </ul>
+          <CSSTransitionGroup
+            transitionName="zkfade"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}
+          >
+            <div className="menu-wrapper violet-gradientbg-light">
+              <div
+                className="menu-wrapper-bg-img"
+                style={_style_menu_wrapper}
+              />
+              <div className="container">
+                <div className="row justify-content-center menu-content-box">
+                  <div className="pos-absolute close-button-holder">
+                    <a
+                      href="javascript:"
+                      className="close-img"
+                      onClick={() => this.closeMenu()}
+                    >
+                      <img src={_img_closebut} alt="" />
+                    </a>
+                  </div>
+                  <div className="col-12">
+                    <ul>
+                      <li>
+                        <Link to="/team">About Us</Link>
+                      </li>
+                      <li>
+                        <Link to="/careers">Careers</Link>
+                      </li>
+                      <li>
+                        <Link to="/faq">FAQ</Link>
+                      </li>
+                      <li>
+                        <Link to="javascript:">Blog</Link>
+                      </li>
+                      <li>
+                        <Link to="/contractor-signup">Become a contractor</Link>
+                      </li>
+                      <li>
+                        <Link to="/contact">Contact Us</Link>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </CSSTransitionGroup>
         </div>
       </div>
     );
--- client/components/Home/innerSections/homeBanner.jsx
+++ client/components/Home/innerSections/homeBanner.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classnames from "classnames";
 
 import style from "./homeBanner.css";
+import { CSSTransitionGroup } from "react-transition-group";
 
 const _img_homeBannerBg = require("../../../images/Home/bitmap.png");
 const _img_mapPin = require("../../../images/bitmap_6.png");
@@ -20,7 +21,7 @@ class HomeBanner extends React.Component {
     return (
       <div>
         <div>
-          <div className="container md-sm-full-width">
+          <div className="container-fluid md-sm-full-width nopadding">
             <div
               className="row homebanner-wrapper"
               style={_style_promoBannerBgImg}
@@ -29,38 +30,46 @@ class HomeBanner extends React.Component {
                 &nbsp;
               </div>
               <div className="col-sm-12 col-md-12 col-lg-5 nopadding">
-                <div className="banner-booking-holder">
-                  <div className="container">
-                    <div className="booking-title">
-                      <p>
-                        Zipc<img src={_img_mapPin} alt="" />de
-                      </p>
-                    </div>
-                    <div className="booking-input-area">
-                      <input
-                        type="text"
-                        placeholder="Enter your zipcode"
-                        className=""
-                      />
-                      <div className="booking-hl-txt">
-                        <span className="icon">
-                          <img src={_img_greenTick} alt="" />
-                        </span>
-                        <span className="txt">
-                          Service is available in your area. Do you want to book
-                          a Cleaning Service.
-                        </span>
+                <CSSTransitionGroup
+                  transitionName="zkSlideIn"
+                  transitionAppear={true}
+                  transitionAppearTimeout={500}
+                  transitionEnter={false}
+                  transitionLeave={false}
+                >
+                  <div className="banner-booking-holder">
+                    <div className="container">
+                      <div className="booking-title">
+                        <p>
+                          Zipc<img src={_img_mapPin} alt="" />de
+                        </p>
                       </div>
-                      <div className="button-holder">
-                        <button className="booking-submit-butt">BOOK</button>
-                        <span className="">
-                          Need assitance feel free to call us -{" "}
-                          <span className="orange-txt">(800) 331-7527</span>
-                        </span>
+                      <div className="booking-input-area">
+                        <input
+                          type="text"
+                          placeholder="Enter your zipcode"
+                          className=""
+                        />
+                        <div className="booking-hl-txt">
+                          <span className="icon">
+                            <img src={_img_greenTick} alt="" />
+                          </span>
+                          <span className="txt">
+                            Service is available in your area. Do you want to
+                            book a Cleaning Service.
+                          </span>
+                        </div>
+                        <div className="button-holder">
+                          <button className="booking-submit-butt">BOOK</button>
+                          <span className="">
+                            Need assitance feel free to call us -{" "}
+                            <span className="orange-txt">(800) 331-7527</span>
+                          </span>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
+                </CSSTransitionGroup>
               </div>
             </div>
           </div>
